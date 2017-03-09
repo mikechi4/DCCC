161eaa6c7c84a4ebdd3da68d93fefc2a084b3e57
Mike <mike.chi4@gmail.com>
2017-03-09 09:22:30 -0500

    response from api is smaller

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,31 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
 
 
-var paths = {
+const paths = {
     jsSource: ['./app.js', './js/**/*.js'],
     scssSource: './styles/**/**/*.scss'
 };
 
 
-gulp.task('js-bundle', function() {
+gulp.task('js-bundle', () => {
     gulp.src(paths.jsSource)
+        .pipe(babel({
+            presets: ['es2015']
+        }))
         .pipe(concat('all.js'))
         .pipe(gulp.dest('./public/dist'))
 });
 
-gulp.task('scss-bundle', function() {
+gulp.task('scss-bundle', () => {
     gulp.src(paths.scssSource)
         .pipe(sass())
         .pipe(concat('styles.css'))
         .pipe(gulp.dest('./public/dist'))
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
     gulp.watch(paths.jsSource, ['js-bundle'])
     gulp.watch(paths.scssSource, ['scss-bundle'])
 });
--- public/dist/all.js
+++ public/dist/all.js
@@ -13,18 +13,29 @@ angular.module('myApp')
 
 angular.module('myApp').service('mainService', function($http, $q){
     var apiUrl = 'https://data.seattle.gov/api/views/cf52-s8er/rows.json';
-
+    var startingIndex = 8;
     this.getData = function(){
       var deferred = $q.defer();
 
       $http({
         method:'GET',
         url: apiUrl
-      }).then(function(response){
-        var data = response.data;
-        deferred.resolve(data);
+      }).then((response) => {
+        var apiData = response.data.data;
+        var condensedData = [];
+
+        // condense the data from the API so we get a smaller array with the info we want
+        apiData.forEach((index) => {
+          var indexData = [];
+          for(var i = 8; i < index.length; i++) {
+            indexData.push(index[i]);
+          }
+          condensedData.push(indexData)
+        })
+
+        deferred.resolve(condensedData);
       })
-      
+
       return deferred.promise;
     }
   })
